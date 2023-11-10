9483099e4f42d12895f336c2f04bd5d078b6e7f5
Lay <senglaypann@gmail.com>
2023-11-10 23:29:27 +0700

    learned to use props

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
+    const title =  'All blog:';
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -9,14 +11,9 @@ const Home = () => {
     
     return (
         <div className="home">
-          {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p>Written by: { blog.author }</p>
-            </div>
-          ))}
+          <BlogList blogs = { blogs } title = { title } />
         </div>
     );
-}
+  }
  
 export default Home;
