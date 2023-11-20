ad9f7c2075ca4758f238e9f79b878d0ae41069c9
Lay <senglaypann@gmail.com>
2023-11-20 16:07:23 +0700

    fetching data

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,23 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const title =  'All blog:';
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('mario');
 
-    const changeName = () => {
-      name === 'mario' ? setName('luigi') : setName('mario');
-    }
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id)
-        setBlogs(newBlogs);
-    }
-
     //useEffect
     useEffect(() => {
-      console.log('used effect')
-    }, [name]);
+      fetch(' http://localhost:8000/blogs')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+    }, []);
     return (
         <div className="home">
-          <BlogList blogs = { blogs } title = { title } handleDelete = { handleDelete } />
-          <p>{ name }</p>
-          <button onClick={ () => changeName() }>change name</button>
+          {blogs && <BlogList blogs = { blogs } title = { title } />}
         </div>
     );
   }
