7c45c1fe40b8c0b38330ed0f2226e9d2ea5f684f
Lay <senglaypann@gmail.com>
2023-11-21 17:39:40 +0700

    exact match routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
@@ -8,8 +9,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
              <Home />
+            </Route>
+            <Route path="/create">
+             <Create />
             </Route>
           </Switch>
         </div>
